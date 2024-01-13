05c0650d6c04e4ab118e937c2a40cb2f2f7735ac
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 19:03:44 +0200

    Update form.blade.php

--- src/resources/views/author/form.blade.php
+++ src/resources/views/author/form.blade.php
@@ -2,33 +2,33 @@
 
 @section('content')
 
- 	<h1>{{ $title }}</h1>
+    <h1>{{ $title }}</h1>
 
- 	@if ($errors->any())
- 		<div class="alert alert-danger">Please fix the errors! </div>
- 	@endif
+    @if ($errors->any())
+        <div class="alert alert-danger">Please fix the errors!</div>
+    @endif
 
- 	<form method="post" action="{{ $author->exists ? '/authors/patch/' . $author->id : '/authors/put' }}">
- 		@csrf
+    <form method="post" action="{{ $author->exists ? '/authors/patch/' . $author->id : '/authors/put' }}">
+        @csrf
 
- 		<div class="mb-3">
- 			<label for="author-name" class="form-label">Author name</label>
+        <div class="mb-3">
+            <label for="author-name" class="form-label">Author name</label>
+            <input
+                type="text"
+                class="form-control @error('name') is-invalid @enderror"
+                id="author-name"
+                name="name"
+                value="{{ old('name', $author->name) }}">
 
- 			<input
- 				type="text"
- 				class="form-control @error('name') is-invalid @enderror"
- 				id="author-name"
- 				name="name"
-				value="{{ old('name', $author->name) }}">
 
- 			@error('name')
- 				<p class="invalid-feedback">{{ $errors->first('name') }}</p>
- 			@enderror
+            @error('name')
+                <p class="invalid-feedback">{{ $errors->first('name') }}</p>
+            @enderror
+        
+        </div>
 
- 		</div>
+        <button type="submit" class="btn btn-primary">Save</button>
 
- 		<button type="submit" class="btn btn-primary">Save</button>
-
- 	</form>
-
+    </form>
+    
 @endsection
