91f2ac49345b5ca5eb5bee209cf814c0a60db089
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 18:43:39 +0200

    Update BookRequest.php

--- src/app/Http/Requests/BookRequest.php
+++ src/app/Http/Requests/BookRequest.php
@@ -6,49 +6,57 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class BookRequest extends FormRequest
 {
-	public function authorize(): bool
-	{
-		return true;
-	}
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
 
-	public function rules(): array
-	{
-        	return [
-			'name' => 'required|min:3|max:256',
-			'author_id' => 'required',
-			'description' => 'nullable',
-			'price' => 'nullable|numeric',
-			'year' => 'numeric',
-			'image' => 'nullable|image',
-			'display' => 'nullable'
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:3|max:256',
+            'author_id' => 'required',
+            'genre_id' => 'required',
+            'description' => 'nullable',
+            'price' => 'nullable|numeric',
+            'year' => 'numeric',
+            'image' => 'nullable|image',
+            'display' => 'nullable'
+        ];
+    }
 
-        	];
-	}
+    public function messages()
+    {
+        return [
+            'required' => 'Lauks ":attribute" ir obligāts',
+            'min' => 'Laukam ":attribute" jābūt vismaz :min simbolus garam',
+            'max' => 'Lauks ":attribute" nedrīkst būt garāks par :max simboliem',
+            'boolean' => 'Lauka ":attribute" vērtībai jābūt "true" vai "false"',
+            'unique' => 'Šāda lauka ":attribute" vērtība jau ir reģistrēta',
+            'numeric' => 'Lauka ":attribute" vērtībai jābūt skaitlim',
+            'image' => 'Laukā ":attribute" jāpievieno korekts attēla fails',
+        ];
+    }
 
-	public function messages()
-	{
- 		return [
- 			'required' => 'Lauks ":attribute" ir obligāts',
- 			'min' => 'Laukam ":attribute" jābūt vismaz :min simbolus garam',
- 			'max' => 'Lauks ":attribute" nedrīkst būt garāks par :max simboliem',
- 			'boolean' => 'Lauka ":attribute" vērtībai jābūt "true" vai "false"',
- 			'unique' => 'Šāda lauka ":attribute" vērtība jau ir reģistrēta',
- 			'numeric' => 'Lauka ":attribute" vērtībai jābūt skaitlim',
- 			'image' => 'Laukā ":attribute" jāpievieno korekts attēla fails',
- 		];
-	}
-
-	public function attributes()
-	{
- 		return [
- 			'name' => 'nosaukums',
- 			'author_id' => 'autors',
- 			'description' => 'apraksts',
- 			'price' => 'cena',
- 			'year' => 'gads',
- 			'image' => 'attēls',
- 			'display' => 'publicēt',
- 		];
-	}
+    public function attributes()
+    {
+        return [
+            'name' => 'nosaukums',
+            'author_id' => 'autors',
+            'description' => 'apraksts',
+            'price' => 'cena',
+            'year' => 'gads',
+            'image' => 'attēls',
+            'display' => 'publicēt',
+        ];
+    }
 
 }
