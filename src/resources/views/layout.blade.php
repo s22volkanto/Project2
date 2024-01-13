e3509bd806aefde7c3a413afb48251ee1914c3a8
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 19:02:23 +0200

    Update layout.blade.php

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -1,77 +1,94 @@
 <!doctype html>
+<html lang="en">
 
-<html lang="lv">
+    <head>
+        <meta charset="utf-8">
+        <title>Project 2 - {{ $title }}</title>
+        <meta name="description" content="Web Technologies Practical Work 2">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-	<meta charset="utf-8">
-	<title>Project 2 - {{ $title }}</title>
-	<meta name="description" content="Tīmekļa Tehnoloģiju 2. praktiskais darbs">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+            crossorigin="anonymous"
+        >
 
-	<link
-		href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
-		rel="stylesheet"
-		integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
-		crossorigin="anonymous"
-	>
+    </head>
 
-</head>
+    <body>
+    <nav class="navbar bg-primary mb-3" data-bs-theme="dark">
+        
+        <header class="container">
 
-<body>
-	<nav class="navbar navbar-expand-md bg-primary mb-3" data-bs-theme="dark">
-		<div class="container">
-			<span class="navbar-brand mb-0 h1">Anton Volkov's Project 2</span>
+            <nav class="navbar navbar-expand-md bg-primary mb-3" data-bs-theme="dark">
+                <div class="container">
+                    <span class="navbar-brand mb-0 h1">Administration</span>
 
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse"data-bs-target="#navbarNav">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+data-bs-target="#navbarNav">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-			<div class="collapse navbar-collapse" id="navbarNav">
-				<ul class="navbar-nav">
-					<li class="nav-item">
-						<a class="nav-link" href="/">Home</a>
-					</li>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/">Home</a>
+                            </li>           
 
-					@if(Auth::check())
+                            @if(Auth::check())
 
-						<li class="nav-item">
-							<a class="nav-link" href="/authors">Authors</a>
-						</li>
-						<li class="nav-item">
- 							<a class="nav-link" href="/books">Books</a>
-						</li>
-						<li class="nav-item">
-                            				<a class="nav-link" href="/genres">Genres</a>
-                        			</li>
-						<li class="nav-item">
- 							<a class="nav-link" href="/logout">Log out</a>
- 						</li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/authors">Authors</a>
+                                </li>
 
-					@else
+                                <li class="nav-item">
+                                <a class="nav-link" href="/books">Books</a>
+                                </li>
 
-						<li class="nav-item">
- 							<a class="nav-link" href="/login">Log in</a>
- 						</li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="/genres">Genre</a>
+                                </li>
 
-					@endif
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/logout">Log out</a>
+                                </li>
 
-				</ul>
-			</div>
-		</div>
-	</nav>
+                                @else
 
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="/login">Log in</a>
+                                    </li>
+                                @endif
+                        </ul>
+                    </div>
+                </div>
+            </nav>
 
-	<footer class="text-bg-dark mt-3">
-		<div class="container">
-			<div class="row py-5">
-				<div class="col">
-					A. Volkov, 2024
+        </header>
+    </nav>
 
-				</div>
-			</div>
-		</div>
-	</footer>
-<script src="/js/admin.js"></script>	
+<main class="container">
+    <div class="row">
+        <div class="col">
+            @yield('content')
+
+        </div>
+    </div>
+</main>
+
+<footer class="text-bg-dark mt-3">
+    <div class="container">
+        <div class="row py-5">
+            <div class="col">
+                 Web Technologies, Project 2 - Anton Volkov, 2024
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="/js/admin.js"></script>
+
 </body>
 
 </html>
