229b7112b1afb7ba3b72d88a2b116c81fdaee5ba
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 18:42:34 +0200

    Update Author.php

--- src/app/Models/Author.php
+++ src/app/Models/Author.php
@@ -7,8 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    	public function books()
-	{
- 		return $this->hasMany(Book::class);
-	}
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function books()
+    {
+         return $this->hasMany(Book::class);
+    }
+
 }
