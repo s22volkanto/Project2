ac5292df87de1d2d9fa497023d088e32ed9539c2
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 18:44:51 +0200

    Update GenreRequest.php

--- src/app/Http/Requests/GenreRequest.php
+++ src/app/Http/Requests/GenreRequest.php
@@ -6,30 +6,38 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class GenreRequest extends FormRequest
 {
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
 
-    	public function authorize(): bool
-    	{
-        	return true;
-    	}
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+        ];
+    }
 
-    	public function rules(): array
-    	{
-        	return [
-            		'name' => 'required',
-        	];
-    	}
+    public function messages()
+    {
+        return [
+            'required' => 'The ":attribute" field is mandatory',
+        ];
+    }
 
-	public function messages()
-    	{
-        	return [
-            		'required' => 'Lauks ":attribute" ir obligāts',
-        	];
-    	}
+    public function attributes()
+    {
+        return [
+            'name' => 'Name',
+        ];
+    }
 
-    	public function attributes()
-    	{
-        	return [
-            	'name' => 'nosaukums',
-        	];
-    	}
 }
