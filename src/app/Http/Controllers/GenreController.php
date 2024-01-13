9b909799849f1aed71754bc813377c22630b39be
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 18:49:51 +0200

    Update GenreController.php

--- src/app/Http/Controllers/GenreController.php
+++ src/app/Http/Controllers/GenreController.php
@@ -7,70 +7,79 @@ use App\Http\Requests\GenreRequest;
 
 class GenreController extends Controller
 {
-	public function __construct()
-    	{
-        	$this->middleware('auth');
-    	}
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-    	private function saveGenreData(Genre $genre, GenreRequest $request)
-    	{
-        	$validatedData = $request->validated();
-        	$genre->fill($validatedData);
+    private function saveGenreData(Genre $genre, GenreRequest $request)
+    {
+        $validatedData = $request->validated();
+        $genre->fill($validatedData);
 
-        	$genre->save();
-    	}
+        $genre->save();
+    }
 
-    	public function list()
-    	{
-        	$items = Genre::orderBy('name', 'asc')->get();
+    // display all genres
+    public function list()
+    {
+        $items = Genre::orderBy('name', 'asc')->get();
 
-        	return view(
-            		'genre.list',
-            		[
-                		'title' => 'Genre',
-                		'items' => $items,
-            		]
-        	);
-    	}
+        return view(
+            'genre.list',
+            [
+                'title' => 'Genres',
+                'items' => $items,
+            ]
+        );
+    }
 
-    	public function create()
-    	{
-        	return view(
-            		'genre.form',
-            		[
-                		'title' => 'Add new genre',
-                		'genre' => new Genre(),
-            		]
-        	);
-    	}
+    // display new genre form
+    public function create()
+    {
+        return view(
+            'genre.form',
+            [
+                'title' => 'Add a new genre',
+                'genre' => new Genre(),
+            ]
+        );
+    }
 
-    	public function put(GenreRequest $request)
-    	{
-        	$genre = new Genre();
-        	$this->saveGenreData($genre, $request);
-        	return redirect('/genres');
-    	}
+    // save new genre
+    public function put(GenreRequest $request)
+    {
+        $genre = new Genre();
 
-    	public function update(Genre $genre)
-    	{
-        	return view(
-            		'genre.form',
-            		[
-                		'title' => 'Edit genre',
-                		'genre' => $genre,
-            		]
-        	);
-    	}
+        $this->saveGenreData($genre, $request);
 
-    	public function patch(Genre $genre, GenreRequest $request)
-    	{
-        	$this->saveGenreData($genre, $request);
-        	return redirect('/genres');
-    	}
+        return redirect('/genres');
+    }
 
-    	public function delete(Genre $genre)
-    	{
-        	$genre->delete();
-        	return redirect('/genres');
-    	}
+    // display genre update form
+    public function update(Genre $genre)
+    {
+        return view(
+            'genre.form',
+            [
+                'title' => 'Edit genre',
+                'genre' => $genre,
+            ]
+        );
+    }
+
+    // update existing genres
+    public function patch(Genre $genre, GenreRequest $request)
+    {
+        $this->saveGenreData($genre, $request);
+
+        return redirect('/genres');
+    }
+
+    // delete genres
+    public function delete(Genre $genre)
+    {
+        $genre->delete();
+        return redirect('/genres');
+    }
 }
