39d2de64ef2369ebb9eaaa4d51f7db0c7e593517
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 18:49:16 +0200

    Update DataController.php

--- src/app/Http/Controllers/DataController.php
+++ src/app/Http/Controllers/DataController.php
@@ -7,39 +7,35 @@ use App\Models\Book;
 
 class DataController extends Controller
 {
-    	// Returns 3 published Book objects in random order
-	public function getTopBooks()
-	{
- 		$books = Book::where('display', true)
- 			->inRandomOrder()
- 			->take(3)
- 			->get();
+    // Returns 3 published Book objects in random order
+    public function getTopBooks()
+    {
+        $books = Book::where('display', true)
+        ->inRandomOrder()
+        ->take(3)
+        ->get();
+        return $books;
+    }
+    // Returns the selected Book object, if it’s published
+    public function getBook(Book $book)
+    {
+        $selectedBook = Book::where([
+        'id' => $book->id,
+        'display' => true,
+        ])
+        ->firstOrFail();
+        return $selectedBook;
+    }
 
- 		return $books;
-	}
-
-	// Returns the selected Book object, if it’s published
-	public function getBook(Book $book)
-	{
- 		$selectedBook = Book::where([
- 				'id' => $book->id,
- 				'display' => true,
- 			])
- 			->firstOrFail();
-
- 		return $selectedBook;
-	}
-
-	// Returns 3 published Book objects in random order- except the selected one
-	public function getRelatedBooks(Book $book)
-	{
- 		$books = Book::where('display', true)
- 			->where('id', '<>', $book->id)
- 			->inRandomOrder()
- 			->take(3)
- 			->get();
-
- 		return $books;
-	}
+    // Returns 3 published Book objects in random order- except the selected one
+    public function getRelatedBooks(Book $book)
+    {
+        $books = Book::where('display', true)
+        ->where('id', '<>', $book->id)
+        ->inRandomOrder()
+        ->take(3)
+        ->get();
+        return $books;
+    }
 
 }
