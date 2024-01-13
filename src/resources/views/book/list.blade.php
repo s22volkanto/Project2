62e79c84108249ef6a5d495c2cc8b62236c4449a
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 19:03:19 +0200

    Update list.blade.php

--- src/resources/views/book/list.blade.php
+++ src/resources/views/book/list.blade.php
@@ -1,59 +1,57 @@
+
 @extends('layout')
 
 @section('content')
 
-<h1>{{ $title }}</h1>
+    <h1>{{ $title }}</h1>
+    <hr>
 
-@if (count($items) > 0)
+    @if (count($items) > 0)
 
- 	<table class="table table-sm table-hover table-striped">
- 		<thead class="thead-light">
- 			<tr>
- 				<th>ID</th>
- 				<th>Name</th>
- 				<th>Author</th>
- 				<th>Year</th>
- 				<th>Price</th>
- 				<th>Published</th>
- 				<th>&nbsp;</th>
- 			</tr>
- 		</thead>
- 		<tbody>
+        <table class="table table-striped table-hover table-sm">
+            <thead class="">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Author</th>
+                    <th>Genre</th>
+                    <th>Year</th>
+                    <th>Price</th>
+                    <th>Published</th>
+                    <th>Activities</th>
+                </tr>
+            </thead>
+            <tbody>
 
- 		@foreach($items as $book)
- 			<tr>
- 				<td>{{ $book->id }}</td>
- 				<td>{{ $book->name }}</td>
- 				<td>{{ $book->author->name }}</td>
- 				<td>{{ $book->year }}</td>
- 				<td>&euro; {{ number_format($book->price, 2, '.') }}</td>
- 				<td>{!! $book->display ? '&#x2714;' : '&#x274C;' !!}</td>
-				<td>
- 					<a
- 						href="/books/update/{{ $book->id }}"
- 						class="btn btn-outline-primary btn-sm"
- 					>Edit</a> /
- 					<form
- 						method="post"
- 						action="/books/delete/{{ $book->id }}"
- 						class="d-inline deletion-form"
- 					>
- 						@csrf
-						<button
- 							type="submit"
- 							class="btn btn-outline-danger btn-sm"
- 						>Delete</button>
- 					</form>
- 				</td>
- 			</tr>
- 		@endforeach
+                @foreach($items as $book)
+                <tr>
+                    <td>{{ $book->id }}</td>
+                    <td>{{ $book->name }}</td>
+                    <td>{{ $book->author->name }}</td>
+                    <td>{{ optional($book->genre)->name }}</td>
+                    <td>{{ $book->year }}</td>
+                    <td>&euro; {{ number_format($book->price, 2, '.') }}</td>
+                    <td>{!! $book->display ? '&#x2714;' : '&#x274C;' !!}</td>
+                    <td>
+                        <a href="/books/update/{{ $book->id }}" class="btn btn-outline-primary btn-sm">Edit</a>
+                        /
+                        <form method="post" action="/books/delete/{{ $book->id }}" class="deletion-form d-inline">
+                            @csrf
+                            <button type="submit" class="btn btn-outline-danger btn-sm">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
 
- 		</tbody>
- 	</table>
-@else
- 	<p>No entries found in database </p>
-@endif
+            </tbody>
+        </table>
 
-<a href="/books/create" class="btn btn-primary">Add new book</a>
+    @else
+
+        <p>No record found in database</p>
+
+    @endif
+
+    <a href="/books/create" class="btn btn-primary">Add a new book</a>
 
 @endsection
