4cb4ba33341f11667dba4de1b7467b3c4eb87e8c
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 19:03:56 +0200

    Update list.blade.php

--- src/resources/views/author/list.blade.php
+++ src/resources/views/author/list.blade.php
@@ -1,37 +1,46 @@
 @extends('layout')
+
 @section('content')
- 	<h1>{{ $title }}</h1>
- 	@if (count($items) > 0)
- 		<table class="table table-striped table-hover table-sm">
- 			<thead class="thead-light">
- 				<tr>
- 					<th>ID</td>
-					<th>Name</td>
-					<th>&nbsp;</td>
- 				</tr>
- 			</thead>
- 			<tbody>
 
- 			@foreach($items as $author)
- 			<tr>
- 				<td>{{ $author->id }}</td>
- 				<td>{{ $author->name }}</td>
- 				<td><a href="/authors/update/{{ $author->id }}" class="btn btn-outline-primary btnsm">Edit</a> / 
-					<form action="/authors/delete/{{ $author->id }}" method="post" class="deletionform d-inline">
- 						@csrf
- 						<button type="submit" class="btn btn-outline-danger btn-sm">Delete</button>
-					</form>
-				</td>
- 			</tr>
- 			@endforeach
+    <h1>{{ $title }}</h1>
 
- 			</tbody>
- 		</table>
+    @if (count($items) > 0)
+ 
+        <table class="table table-striped table-hover table-sm">
+            <thead class="thead-light">
+                <tr>
+                    <th>ID</td>
+                    <th>Name</td>
+                    <th>&nbsp;</td>
+                </tr>
+            </thead>
+            <tbody>
 
-	<a href="/authors/create" class="btn btn-primary">Izveidot jaunu</a>
+            @foreach($items as $author)
+            <tr>
+                <td>{{ $author->id }}</td>
+                <td>{{ $author->name }}</td>
+                <td>
+                    <a href="/authors/update/{{ $author->id }}" class="btn btn-outline-primary btn-sm">Edit</a>
 
- 	@else
- 		<p>No entries found in database</p>
- 	@endif
+                    <form action="/authors/delete/{{ $author->id }}" method="post" class="deletion-form d-inline">
+                        @csrf
+                        <button type="submit" class="btn btn-outline-danger btn-sm">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
 
+            </tbody>
+    </table>
+
+    <!-- Add the link below the table -->
+    <a href="/authors/create" class="btn btn-primary">Create New</a>
+
+    @else
+
+    <p>No entries found in database</p>
+
+    @endif
+    
 @endsection
