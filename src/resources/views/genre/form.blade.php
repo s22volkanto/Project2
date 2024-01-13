dd462840d2f9a9c2e8a21c55a1f68eda9f039461
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 19:00:40 +0200

    Update form.blade.php

--- src/resources/views/genre/form.blade.php
+++ src/resources/views/genre/form.blade.php
@@ -1,38 +1,39 @@
+
 @extends('layout')
 
 @section('content')
 
-    	<h1>{{ $title }}</h1>
-    	<hr>
+    <h1>{{ $title }}</h1>
+    <hr>
 
-    	@if ($errors->any())
-        	<div class="alert alert-danger" role="alert">
-            		Please fix the errors!
-        	</div>
-    	@endif
+    @if ($errors->any())
+        <div class="alert alert-danger" role="alert">
+            Please fix the errors!
+        </div>
+    @endif
 
 
-    	<form method="post" action="{{ $genre->exists ? '/genres/patch/' . $genre->id : '/genres/put' }}">
-        	@csrf
+    <form method="post" action="{{ $genre->exists ? '/genres/patch/' . $genre->id : '/genres/put' }}">
+        @csrf
 
-        		<div class="mb-3">
-            			<label for="genre-name" class="form-label">Genre name</label>
+        <div class="mb-3">
+            <label for="genre-name" class="form-label">Genre name</label>
 
-            			<input
-                			type="text"
-                			id="genre-name"
-                			name="name"
-                			class="form-control @error('name') is-invalid @enderror"
-                			value="{{ old('name', $genre->name) }}"
-            			>
+            <input
+                type="text"
+                id="genre-name"
+                name="name"
+                class="form-control @error('name') is-invalid @enderror"
+                value="{{ old('name', $genre->name) }}"
+            >
 
-            			@error('name')
-                			<p class="invalid-feedback">{{ $errors->first('name') }}</p>
-            			@enderror
-        		</div>
+            @error('name')
+                <p class="invalid-feedback">{{ $errors->first('name') }}</p>
+            @enderror
+        </div>
 
-        		<button type="submit" class="btn btn-primary">{{ $genre->exists ? 'Restore' : 'Add' }}</button>
+        <button type="submit" class="btn btn-primary">{{ $genre->exists ? 'Update' : 'Create' }}</button>
 
-    	</form>
+    </form>
 
 @endsection
