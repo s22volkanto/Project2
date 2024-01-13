cc094db1ea38caebe9f66d58dc930b29db52d3cc
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 18:58:58 +0200

    Update 2024_01_13_133214_create_genres_table.php

--- src/database/migrations/2024_01_13_133214_create_genres_table.php
+++ src/database/migrations/2024_01_13_133214_create_genres_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('genres', function (Blueprint $table) {
             $table->id();
-	    $table->string('name', 256);
+            $table->string('name', 256);
             $table->timestamps();
         });
     }
