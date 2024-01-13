ce6879750a2f62811c58083e9c6d2da0e117ff79
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 18:41:58 +0200

    Update Book.php

--- src/app/Models/Book.php
+++ src/app/Models/Book.php
@@ -7,26 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-	use HasFactory;
+    use HasFactory;
 
-	protected $fillable = ['name', 'author_id', 'description', 'price', 'year'];
+    protected $fillable = ['name', 'author_id', 'genre_id', 'description', 'price', 'year'];
 
     public function author()
-	{
- 		return $this->belongsTo(Author::class);
-	}
+    {
+        return $this->belongsTo(Author::class);
+    }
 
-	public function jsonSerialize(): mixed
-	{
- 		return [
- 			'id' => intval($this->id),
- 			'name' => $this->name,
- 			'description' => $this->description,
- 			'author' => $this->author->name,
- 			'genre' => ($this->genre ? $this->genre->name : ''),
- 			'price' => number_format($this->price, 2),
- 			'year' => intval($this->year),
- 			'image' => asset('images/' . $this->image),
- 		];
-	}
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function jsonSerialize(): mixed
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'author' => $this->author->name,
+            'genre' => ($this->genre ? $this->genre->name : ''),
+            'price' => floatval(number_format($this->price, 2)),
+            'year' => $this->year,
+            'image' => asset('images/' . $this->image),
+        ];
+    }
+
 }
