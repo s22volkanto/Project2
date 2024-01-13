1e9fcb63fbd405c541994834d2bf7a731f531b2e
s22volkanto <149880428+s22volkanto@users.noreply.github.com>
2024-01-13 19:01:00 +0200

    Update list.blade.php

--- src/resources/views/genre/list.blade.php
+++ src/resources/views/genre/list.blade.php
@@ -2,45 +2,45 @@
 
 @section('content')
 
-    	<h1>{{ $title }}</h1>
-    	<hr>
+    <h1>{{ $title }}</h1>
+    <hr>
 
-    	@if (count($items) > 0)
+    @if (count($items) > 0)
 
-        	<table class="table table-striped table-hover table-sm">
-            		<thead class="">
-                		<tr>
-                    			<th>ID</th>
-                    			<th>Title</th>
-                    			<th>Actions</th>
-                		</tr>
-            		</thead>
-            		<tbody>
+        <table class="table table-striped table-hover table-sm">
+            <thead class="">
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
 
-                		@foreach($items as $genre)
-                		<tr>
-                   			<td>{{ $genre->id }}</td>
-                    			<td>{{ $genre->name }}</td>
-                    			<td>
-                        			<a href="/genres/update/{{ $genre->id }}" class="btn btn-outline-primary btn-sm">Edit</a>
-                        			/
-                        			<form method="post" action="/genres/delete/{{ $genre->id }}" class="deletion-form d-inline">
-                           				 @csrf
-                            				<button type="submit" class="btn btn-outline-danger btn-sm">Delete</button>
-                        			</form>
-                    			</td>
-                		</tr>
-                		@endforeach
+                @foreach($items as $genre)
+                <tr>
+                    <td>{{ $genre->id }}</td>
+                    <td>{{ $genre->name }}</td>
+                    <td>
+                        <a href="/genres/update/{{ $genre->id }}" class="btn btn-outline-primary btn-sm">Edit</a>
+                        /
+                        <form method="post" action="/genres/delete/{{ $genre->id }}" class="deletion-form d-inline">
+                            @csrf
+                            <button type="submit" class="btn btn-outline-danger btn-sm">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
 
-            		</tbody>
-        	</table>
+            </tbody>
+        </table>
 
-    	@else
+    @else
 
-        	<p>No record found</p>
+        <p>No record found</p>
 
-    	@endif
+    @endif
 
-    	<a href="/genres/create" class="btn btn-primary">Add new genre</a>
+    <a href="/genres/create" class="btn btn-primary">Add a new genre</a>
 
 @endsection
